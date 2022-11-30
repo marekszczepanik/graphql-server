33b558604e75a1858a41d837f6b12cdaa338e0ad
marekszczepanik <markus.szczepanik@gmail.com>
2022-11-30 10:24:10 +0100

    Apollo server configuration

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Welcome in GraphQL server","Chyba działa");
+const { ApolloServer } = require("apollo-server");
+const typeDefs = `
+    type Query {
+        pozdrowienie: String
+    }
+`;
+const serwerek = new ApolloServer({ typeDefs });
+serwerek.listen({port:4005}).then((wynik) => console.log(wynik.url));
